4d3ba1c7b42cd16a58c6a7d7591aa43dd8d124fd
tcja <cloackage@hotmail.com>
2019-04-26 19:14:53 +0200

    Add files via upload

---
+++ ALL IN ONE Executor.bat
@@ -0,0 +1,12 @@
+REG IMPORT "Disable_Action_Center.reg"
+REG IMPORT "QoS_Limiter.reg"
+REG IMPORT "SSD_Optimizations.reg"
+@echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+@echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+@echo .
+@echo !!!!! REGISTRY OPTIMISATIONS DONE !!!!!
+@echo .
+@echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+@echo -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+call "OneDrive_Uninstaller_v1.2.bat"
+@pause
