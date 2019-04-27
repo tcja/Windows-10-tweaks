47b604f87537ffdae6332b7d8fa51d1dfad820f2
tcja <cloackage@hotmail.com>
2019-04-27 21:49:35 +0200

    Add files via upload

---
+++ RemoveXboxAppsBloat.bat
@@ -0,0 +1,23 @@
+reg add "HKLM\System\CurrentControlSet\Services\xbgm" /v "Start" /t REG_DWORD /d "4" /f
+sc config XblAuthManager start= disabled
+sc config XblGameSave start= disabled
+sc config XboxGipSvc start= disabled
+sc config XboxNetApiSvc start= disabled
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /Disable
+takeown /f "%WinDir%\System32\GameBarPresenceWriter.exe" /a
+icacls "%WinDir%\System32\GameBarPresenceWriter.exe" /grant:r Administrators:F /c
+taskkill /im GameBarPresenceWriter.exe /f
+move "C:\Windows\System32\GameBarPresenceWriter.exe" "C:\Windows\System32\GameBarPresenceWriter.OLD"
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /Disable
+takeown /f "%WinDir%\System32\bcastdvr.exe" /a
+icacls "%WinDir%\System32\bcastdvr.exe" /grant:r Administrators:F /c
+taskkill /im bcastdvr.exe /f
+move C:\Windows\System32\bcastdvr.exe C:\Windows\System32\bcastdvr.OLD
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\GameBar" /v "UseNexusForGameBarEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AudioCaptureEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "CursorCaptureEnabled" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\GameBar" /v "ShowStartupPanel" /t REG_DWORD /d "0" /f
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "0" /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\GameDVR" /v "AllowgameDVR" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\GameBar" /v "AllowAutoGameMode" /t REG_DWORD /d "0" /f
